23506d184bd18276677fed6e691ff5bd98d0681f
Cristian Suarez <cristian.suarez.ulpgc@gmail.com>
2020-04-19 13:00:16 +0100

    Inject infra into write function

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ from sys import argv
 from geo_api import geo_api
 
 
-def write_all_postal_codes(province_id, province_name, output):
-    geo = geo_api(key)
-    municipios = geo.get_municipios(province_id)
+def write_all_postal_codes(infrastructure, province_id, province_name, output):
+    municipios = infrastructure.get_municipios(province_id)
 
     for municipio in municipios:
         print(f"-> {municipio['name']}")
-        poblaciones = geo.get_poblacion(province_id, municipio['id'])
+        poblaciones = infrastructure.get_poblacion(province_id, municipio['id'])
 
         for poblacion in poblaciones:
             print(f"\t-> {poblacion['name']}")
-            nucleos = geo.get_nucleo(province_id, municipio['id'], poblacion['name'])
+            nucleos = infrastructure.get_nucleo(province_id, municipio['id'], poblacion['name'])
 
             for nucleo in nucleos:
                 print(f"\t\t-> {nucleo['name']}")
-                codigos_postales = geo.get_codigo_postal(province_id, municipio['id'], nucleo['id'])
+                codigos_postales = infrastructure.get_codigo_postal(province_id, municipio['id'], nucleo['id'])
 
                 for codigo_postal in codigos_postales:
                     output.write(
@@ -32,6 +31,6 @@ try:
     province_name = geo.get_nombre_provincia(id_provincia)
     file = open(f'codigos_postales/{province_name}.csv', 'w')
     file.write('Provincia; Municipio; Poblacion; Nucleo; Codigo postal;\n')
-    write_all_postal_codes(id_provincia, province_name, file)
+    write_all_postal_codes(geo, id_provincia, province_name, file)
 finally:
     file.close()
